5529a98859edd2b69e378eac097d7f573e06a590
Relax-FM <arton.2@mail.ru>
2022-11-09 04:09:38 +0300

    add needed files

--- blueprint_query/routes.py
+++ blueprint_query/routes.py
@@ -20,29 +20,71 @@ def provider_test():
     return 'None'
 
 
-@blueprint_query.route('/query1', methods=['GET', 'POST'])
-@group_required
-def query1():
-    print(os.path.join(os.path.dirname(__file__)))
-    if request.method == 'POST':
-        input_product = request.form.get('product_name')
-        if input_product:
-            _sql = provider.get('product.sql', input_product=input_product)
-            product_result, schema = select(current_app.config['db_config'], _sql)
-            if len(product_result) == 0:
-                return render_template('not_found.html')
-            return render_template('db_result.html', schema=schema, result=product_result)
-        else:
-            return render_template('not_found.html')
-    elif request.method == 'GET':
-        return render_template('query1.html')
+# @blueprint_query.route('/query1', methods=['GET', 'POST'])
+# @group_required
+# def query1():
+#     print(os.path.join(os.path.dirname(__file__)))
+#     if request.method == 'POST':
+#         input_product = request.form.get('product_name')
+#         if input_product:
+#             _sql = provider.get('product.sql', input_product=input_product)
+#             product_result, schema = select(current_app.config['db_config'], _sql)
+#             if len(product_result) == 0:
+#                 return render_template('not_found.html')
+#             return render_template('db_result.html', schema=schema, result=product_result)
+#         else:
+#             return render_template('not_found.html')
+#     elif request.method == 'GET':
+#         return render_template('query.html')
 
 @blueprint_query.route('/menu_queries')
 @group_required
 def menu_queries():
     return render_template('menu_queries.html')
 
+@blueprint_query.route('/query1')
+@group_required
+def query1():
+    print(os.path.join(os.path.dirname(__file__)))
+    _sql = provider.get('check_stock.sql')
+    product_result, schema = select(current_app.config['db_config'], _sql)
+    if len(product_result) == 0:
+        return render_template('not_found.html')
+    return render_template('db_result.html', schema=['Номер товара', 'Масса', 'Количество', 'Наименование'], result=product_result, query_numb="Query1")
+
 @blueprint_query.route('/query2')
 @group_required
 def query2():
-    return render_template('query2.html')
+    print(os.path.join(os.path.dirname(__file__)))
+    _sql = provider.get('free_delivery.sql')
+    product_result, schema = select(current_app.config['db_config'], _sql)
+    if len(product_result) == 0:
+        return render_template('not_found.html')
+    return render_template('db_result.html', schema=['Номер заказа', 'Дата', 'Имя', 'Фамилия', 'Адрес', 'Номер телефона'], result=product_result, query_numb="Query2")
+
+@blueprint_query.route('/query3', methods=['GET', 'POST'])
+@group_required
+def query3():
+    print(os.path.join(os.path.dirname(__file__)))
+    _sql = provider.get('end_delivery.sql')
+    product_result, schema = select(current_app.config['db_config'], _sql)
+    if len(product_result) == 0:
+        return render_template('not_found.html')
+    return render_template('db_result.html', schema=['Номер заказа', 'Дата', 'Имя', 'Фамилия', 'Адрес', 'Номер телефона'], result=product_result, query_numb="Query3")
+
+@blueprint_query.route('/query4', methods=['GET', 'POST'])
+@group_required
+def query4():
+    print(os.path.join(os.path.dirname(__file__)))
+    if request.method == 'POST':
+        input_product = request.form.get('product_name')
+        if input_product:
+            _sql = provider.get('delivery_details.sql', input_product=input_product)
+            product_result, schema = select(current_app.config['db_config'], _sql)
+            if len(product_result) == 0:
+                return render_template('not_found.html')
+            return render_template('db_result.html', schema=['Номер заказа', 'Дата', 'Клиент', 'Масса', 'Количество', 'Наименование'], result=product_result, query_numb="Query4")
+        else:
+            return render_template('not_found.html')
+    elif request.method == 'GET':
+        return render_template('query.html', ph_title="Введите номер заказа")
